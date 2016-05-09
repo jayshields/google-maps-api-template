38026737baae4f33652ab6d7311e089ec4418622
Jamie Shields <jayshields@gmail.com>
2016-05-09 11:14:25 +0100

    Add markers functionality.

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
-$(document).ready(function(){
-  if($('#map').length > 0)
+$(document).ready(function () {
+  if ($('#map').length > 0)
   {
-    if($('html').attr('lang'))
+    if ($('html').attr('lang'))
       lang = $('html').attr('lang');
     else
       lang = 'en';
-    
-    $.getScript('https://maps.googleapis.com/maps/api/js?callback=initMap&signed_in=true&language='+lang);
+
+    $.getScript('https://maps.googleapis.com/maps/api/js?callback=initMap&signed_in=true&language=' + lang);
   }
 });
 
 var map;
-function initMap() {
+
+function initMap() 
+{
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
+  });
+
+  plotMarkers();
+}
+
+var markers;
+var bounds;
+
+function plotMarkers()
+{
+  markers = [];
+  bounds = new google.maps.LatLngBounds();
+  
+  $.get('markers.json', function (data) {
+    $.each(data, function(){
+      var position = new google.maps.LatLng(this.lat, this.lng);
+      
+      markers.push(
+        new google.maps.Marker({
+          position: position,
+          map: map,
+          animation: google.maps.Animation.DROP
+        })        
+      );
+    
+      bounds.extend(position);
+    });
+    
+    map.fitBounds(bounds);
   });
 }
