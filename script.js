279a5f5cffb34002704b569d2dffe2854c17790a
Jamie Shields <jayshields@gmail.com>
2016-05-12 11:03:44 +0100

    Remove all uses of jQuery

--- script.js
+++ script.js
@@ -1,50 +1,53 @@
-$(document).ready(function () {
-  if ($('#map').length > 0)
+document.addEventListener('DOMContentLoaded', function () {
+  if (document.querySelectorAll('#map').length > 0)
   {
-    if ($('html').attr('lang'))
-      lang = $('html').attr('lang');
+    if (document.querySelector('html').lang)
+      lang = document.querySelector('html').lang;
     else
       lang = 'en';
 
-    $.getScript('https://maps.googleapis.com/maps/api/js?callback=initMap&signed_in=true&language=' + lang);
+    var js_file = document.createElement('script');
+    js_file.type = 'text/javascript';
+    js_file.src = 'https://maps.googleapis.com/maps/api/js?callback=initMap&signed_in=true&language=' + lang;
+    document.getElementsByTagName('head')[0].appendChild(js_file);
   }
 });
 
 var map;
 
-function initMap() 
+function initMap()
 {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
   });
 
-  plotMarkers();
+  fetch('markers.json')
+    .then(response => response.json())
+    .then(plotMarkers);
 }
 
 var markers;
 var bounds;
 
-function plotMarkers()
+function plotMarkers(m)
 {
   markers = [];
   bounds = new google.maps.LatLngBounds();
-  
-  $.get('markers.json', function (data) {
-    $.each(data, function(){
-      var position = new google.maps.LatLng(this.lat, this.lng);
-      
-      markers.push(
-        new google.maps.Marker({
-          position: position,
-          map: map,
-          animation: google.maps.Animation.DROP
-        })        
-      );
-    
-      bounds.extend(position);
-    });
-    
-    map.fitBounds(bounds);
-  }, 'json');
+
+  m.forEach(function (marker) {
+    var position = new google.maps.LatLng(marker.lat, marker.lng);
+
+    markers.push(
+      new google.maps.Marker({
+        position: position,
+        map: map,
+        animation: google.maps.Animation.DROP
+      })
+    );
+
+    bounds.extend(position);
+  });
+
+  map.fitBounds(bounds);
 }
